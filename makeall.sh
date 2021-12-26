52230d4912f3bc6035973ad01e6aaba7cf3e1d72
gswangmd-gssi <gssiwmd@gmail.com>
2021-12-26 08:49:39 +0800

    modified:   makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -9,6 +9,8 @@ git pull
 ./scripts/feeds update -a && ./scripts/feeds install -a
 cp -rf ../openwrt-action/wmd.config .config
 cp -rf ../openwrt-action/zzz-default-settings-wmd ./package/lean/default-settings/files/zzz-default-settings
+#sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.4/g' ./target/linux/x86/Makefile
+
 make defconfig
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s
 #PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j1 V=s 
