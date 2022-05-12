5736d851f43819dc3ac6c0d43296affa05ac33fc
gswangmd-gssi <gssiwmd@gmail.com>
2022-05-13 05:58:20 +0800

    modified:   amlogic.sh 	modified:   r4s.config 	modified:   r4s.sh 	deleted:    r4sfiles/etc/config/firewall 	deleted:    zzz-default-settings-amlogic 	deleted:    zzz-default-settings-r4s

--- amlogic.sh
+++ amlogic.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "compile amlogic for wangMingde......"
+rm -rf tmp
 git pull
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./files/*
 cp -rf ../openwrt-action/amlogicfiles/* ./files
 cp -rf ../openwrt-action/amlogic.config .config
-cp -rf ../openwrt-action/zzz-default-settings-amlogic ./package/lean/default-settings/files/zzz-default-settings
+
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon1806/g' ./feeds/luci/collections/luci/Makefile
 # Add autocore support for armvirt
@@ -16,8 +17,8 @@ echo "DISTRIB_SOURCECODE='lede'" >> ./package/base-files/files/etc/openwrt_relea
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.252）
 sed -i 's/192.168.1.1/10.10.10.252/g' ./package/base-files/files/bin/config_generate
 make defconfig
-# PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j1 V=s 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j1 V=s 
 
 exit 0
 
