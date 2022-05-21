d3a5405de4c22f087053f717313e9c0340a2b148
gswangmd-gssi <gssiwmd@gmail.com>
2022-05-21 21:25:31 +0800

    modified:   configs/r4s.config 	modified:   r4s.sh

--- r4s.sh
+++ r4s.sh
@@ -6,18 +6,26 @@ rm -rf ./files/*
 cp -rf ../openwrt-action/r4sfiles/* ./files
 cp -rf ../openwrt-action/configs/r4s.config .config
 
+# Modify default linux KERNEL
+sed -i 's/KERNEL_PATCHVER=5.15/KERNEL_PATCHVER=5.4/g' ./target/linux/rockchip/Makefile
+
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon-1806/g' ./feeds/luci/collections/luci/Makefile
+
 # Add autocore support for armvirt
-sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' ./package/lean/autocore/Makefile
+#sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' ./package/lean/autocore/Makefile
+
 # Set etc/openwrt_release
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" ./package/lean/default-settings/files/zzz-default-settings
 echo "DISTRIB_SOURCECODE='lede'" >> ./package/base-files/files/etc/openwrt_release
+
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 10.10.10.254）
 sed -i 's/192.168.1.1/10.10.10.254/g' ./package/base-files/files/bin/config_generate
+
 make defconfig
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s
-# PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j1 V=s 
+
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j$(($(nproc) + 1)) V=s
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin make -j1 V=s 
 
 exit 0
 
